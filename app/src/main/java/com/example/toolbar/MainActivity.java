279ec1972e508ce22d6076609d09c236e0d9845c
nikola <nikola721@gmail.com>
2023-05-18 11:13:48 +0500

    Adding a toolbar projectPlease enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/com/example/toolbar/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("My Toolbar");
+        }
+
+
+        toolbar.setSubtitle("Sub    Toolbar");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.opt_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+        if (itemID == R.id.new_folder){
+            Toast.makeText(this, "Create New File", Toast.LENGTH_SHORT).show();
+        }
+        else if (itemID == R.id.open_folder) {
+            Toast.makeText(this, "Open File", Toast.LENGTH_SHORT).show();
+        } else if (itemID == android.R.id.home) {
+            Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Save File", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
